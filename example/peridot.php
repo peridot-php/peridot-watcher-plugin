e00ff0e0d5a92e5f72429621ac25a33dec7693aa
Brian Scaturro <scaturrob@gmail.com>
2014-11-12 11:26:55 -0500

    track multiple file events

--- example/peridot.php
+++ example/peridot.php
@@ -1,8 +1,10 @@
 <?php
 use Evenement\EventEmitterInterface;
+use Peridot\Plugin\Watcher\WatcherInterface;
 use Peridot\Plugin\Watcher\WatcherPlugin;
 
 return function(EventEmitterInterface $emitter) {
     $watcher = new WatcherPlugin($emitter);
     $watcher->track(__DIR__ . '/../src');
+    $watcher->setEvents([WatcherInterface::MODIFY_EVENT, WatcherInterface::CREATE_EVENT]);
 };
